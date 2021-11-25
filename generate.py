3a4f705caefdcbb41d8b72fa7951f8d30095b535
billzyx <billzyx11@gmail.com>
2021-11-25 03:02:10 -0500

    add get paper format online

--- generate.py
+++ generate.py
@@ -1,37 +1,119 @@
 import os
 import argparse
+from title2bib.crossref import get_bib_from_title
+from pdfrw import PdfReader
+from bibtexparser.bparser import BibTexParser
 
 
-def get_directory_structure(root_dir, out_file='paper.md'):
-    with open(out_file, 'w') as f:
+def load_paper_dicts(root_dir):
+    paper_dict_list = []
+
+    for path, dirs, files in sorted(os.walk(root_dir)):
+        paper_list = [os.path.join(path, file) for file in files if file.endswith('.pdf')]
+        if paper_list:
+            folders = path.replace(root_dir, '').split(os.sep)
+            paper_dict = dict()
+            paper_dict['classes'] = folders
+            paper_dict['paper_list'] = paper_list
+            paper_dict_list.append(paper_dict)
+    return paper_dict_list
+
+
+class PaperParser:
+    def _get_paper_title(self, file_path):
+        # Use filename as paper title if get filename from pdf failed.
+        reader = PdfReader(file_path)
+        title = str(reader.Info.Title)
+        if title is None or title == '' or title == '()' or title == 'None' or title == '(Untitled)':
+            title = os.path.basename(file_path).replace('.pdf', '')
+        else:
+            title = title.strip('()')
+        return title
+
+    def _get_bib_string(self, title):
+        _, bib = get_bib_from_title(title, get_first=True, abbrev_journal=False)
+        return bib
+
+    def _get_paper_info(self, bib):
+        bib_tex_parser = BibTexParser()
+        bib_database = bib_tex_parser.parse(bib)
+
+        paper_info = bib_database.entries[0]
+        return paper_info
+
+    def _get_paper_string_md(self, paper_info):
+        if hasattr(paper_info, 'journal'):
+            journal = paper_info['journal'].replace('{', '').replace('}', '')
+        else:
+            journal = paper_info['booktitle']
+
+        paper_str = '- [{title}]({url}) - {author}, {journal}, ({year})'.format(
+            title=paper_info['title'], url=paper_info['url'], author=paper_info['author'],
+            journal=journal, year=paper_info['year'])
+
+        return paper_str
+
+    def parse(self, paper_path):
+        paper_title = self._get_paper_title(paper_path)
+        paper_bib = self._get_bib_string(paper_title)
+        paper_info = self._get_paper_info(paper_bib)
+        paper_str = self._get_paper_string_md(paper_info)
+        return paper_str
+
+
+
+
+def parse_paper_dicts(paper_dict_list):
+    paper_parser = PaperParser()
+    for i in range(len(paper_dict_list)):
+        paper_str_list = []
+        paper_list = paper_dict_list[i]['paper_list']
+        for paper_path in paper_list:
+            try:
+                paper_str = paper_parser.parse(paper_path)
+                paper_str_list.append(paper_str)
+            except:
+                print('Error getting paper:', paper_path)
+        paper_dict_list[i]['paper_str_list'] = paper_str_list
+    return paper_dict_list
+
+
+def generate_output_md(paper_dict_list, output_md='paper.md', header_index=2):
+    with open(output_md, 'w') as f:
         f.write('## Papers\n')
-        pre_folders = ['']
-        for path, dirs, files in sorted(os.walk(root_dir)):
-            # print(path, dirs, files)
-            paper_list = [file.replace('.pdf', '') for file in files if file.endswith('.pdf')]
-            if paper_list:
-                folders = path.replace(root_dir, '').split(os.sep)
-                folders_print = [x for x in folders if x not in pre_folders]
-                for i, folder in enumerate(folders_print):
-                    title = '#' * (folders.index(folder) + 2) + ' ' + folder
-                    print(title)
-                    f.write(title)
-                    f.write('\n')
-                print(paper_list)
-                for paper in paper_list:
-                    f.write('- ' + paper)
-                    f.write('\n')
-                pre_folders = folders
+        pre_classes = ['']
+        for paper_dict in paper_dict_list:
+            classes = paper_dict['classes']
+            classes_print = [x for x in classes if x not in pre_classes]
+            for paper_class in classes_print:
+                title = '#' * (classes.index(paper_class) + header_index) + ' ' + paper_class
+                f.write(title)
+                f.write('\n')
+            paper_str_list = paper_dict['paper_str_list']
+            for paper_str in paper_str_list:
+                f.write('- ' + paper_str)
+                f.write('\n')
+            pre_classes = classes
 
 
 def main():
     ap = argparse.ArgumentParser()
     ap.add_argument(
-        "--root_dir", required=True,
+        "--paper_dir", required=True,
         default=None,
     )
+    ap.add_argument(
+        "--output_md", required=False,
+        default='paper.md',
+    )
+    ap.add_argument(
+        "--header_index", required=False, type=int,
+        default=2,
+    )
     args = vars(ap.parse_args())
-    get_directory_structure(args['root_dir'])
+    paper_dict_list = load_paper_dicts(args['paper_dir'])
+    paper_dict_list = parse_paper_dicts(paper_dict_list)
+    generate_output_md(paper_dict_list, args['output_md'], args['header_index'])
 
 
 if __name__ == '__main__':
---
+++ title2bib/crossref.py
@@ -0,0 +1,103 @@
+
+from __future__ import unicode_literals, print_function, absolute_import
+import requests
+from builtins import input
+import difflib
+from doi2bib.crossref import get_bib_from_doi
+from unidecode import unidecode
+from arxivcheck.arxiv import get_arxiv_info, generate_bib_from_arxiv
+bare_url = "http://api.crossref.org/"
+
+
+def find_cross_info(params):
+    url = bare_url+"works"
+    requested = requests.get(url, params=params)
+    return requested
+
+
+def ask_which_is(title, items):
+    found = False
+    result = {}
+    question = "\t It is >>'{}' article?\n y(yes)|n(no)|q(quit)"
+    for item in items:
+        w = input(question.format(
+            unidecode(item["title"]), unidecode(title)))
+        if w == "y":
+            found = True
+            result = item
+            break
+        if w == "q":
+            break
+    return found, result
+
+
+def sort_items_by_title(items, title):
+    return sorted(
+        items,
+        key=lambda x: difflib.SequenceMatcher(
+            None,
+            x["title"],
+            title
+        ).ratio(),
+        reverse=True
+    )
+
+
+def get_from_title(title, get_first=False):
+    found = False
+    params = {"query.bibliographic": title, "rows": 20}
+    r = find_cross_info(params)
+    items = r.json()["message"]["items"]
+    for i, item in enumerate(items):
+        title_item = item["title"][0]
+        try:
+            title_item = title_item.decode("utf-8")
+        except:
+            pass
+
+        item["title"] = title_item
+        item["is_crossref"] = True
+
+        if title_item == title:
+            return True, item
+        
+        items[i] = item
+
+    if len(items) == 0:
+        found_arxiv,  arxiv_items = get_arxiv_info(title, field="ti")
+        for arxiv_item in arxiv_items:
+            arxiv_item["is_crossref"] = False
+            items.append(arxiv_item)
+
+    if r.status_code == 200 and len(items) > 0:
+        items = sort_items_by_title(items, title)
+        if get_first:
+            found = True
+            item = items[0]
+        else:
+            found, item = ask_which_is(title, items)
+
+    return found, item
+
+
+def get_bib_from_title(title, get_first=False, abbrev_journal=False):
+    found = False
+    bib = ""
+    found, item = get_from_title(title, get_first)
+    if found:
+        if item["is_crossref"]:
+            if "DOI" in item:
+                doi = item["DOI"]
+                found, bib = get_bib_from_doi(doi, abbrev_journal=abbrev_journal)
+
+        else:
+            if "arxiv_doi" in item:
+                doi = item["arxiv_doi"]
+                published, bib = get_bib_from_doi(doi, abbrev_journal)
+            else:
+                bib = generate_bib_from_arxiv(item, title, field="ti")
+
+            # if "short-container-title" in item:
+            # abbreviated_journal = item["short-container-title"][0]##aqui pode acontecer de realizar a chamada para
+
+    return found, bib
